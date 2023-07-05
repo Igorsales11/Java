c5489420f3a80f683dfa91a0fc67f04a5af236e1
Igorsales11 <igor48sales@gmail.com>
2023-07-05 14:37:20 -0300

    ProjetoFinalizado

--- src/main/java/com/gerenciadordefuncionarios/controller/FuncionarioController.java
+++ src/main/java/com/gerenciadordefuncionarios/controller/FuncionarioController.java
@@ -2,12 +2,18 @@ package com.gerenciadordefuncionarios.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.gerenciadordefuncionarios.repository.FuncionarioRepository;
+
+
 import com.gerenciadordefuncionarios.model.Funcionario;
 
 @Controller
@@ -28,8 +34,62 @@ public class FuncionarioController {
 		model.addAttribute("funcionarios", funcionarios);
 		
 		return "Funcionario";
+	}	
+	
+	
+	@GetMapping("/adicionarfuncionario")
+	public String adicionarFuncionario(Funcionario funcionario) {
+		return "adicionar_funcionario";
+	}
+	
+	@PostMapping("/addfuncionario")
+	public String adicionaFuncionario(@Valid Funcionario funcionario
+			, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			return "/adicionar_funcionario";
+		}
+		FuncionarioRepository.save(funcionario);
+		return "redirect:/Funcionario";
 	}
 	
 	
+					 
+	@GetMapping("/editar/{id}")
+	public String mostrarFormularioAtualizacao(
+	@PathVariable("id") long id, Model model) {
+		
+		Funcionario funcionario = FuncionarioRepository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("Identificador não é válido" + id));
+				model.addAttribute("funcionario", funcionario);
+		return "editar_funcionario";
+	}
+	
+	
+	
+	@PostMapping("/atualizar/{id}")
+	public String atualizaFuncionario(@PathVariable("id")
+		long id, @Valid Funcionario funcionario
+		, BindingResult result, Model model
+			) {
+		if(result.hasErrors()) {
+			funcionario.setId(id);
+			return "editar_funcionario";
+		}
+		FuncionarioRepository.save(funcionario);
+		return "redirect:/Funcionario";
+	}
+	
+	
+	
+	
+	
+	@GetMapping("/delete/{id}")
+	public String deletarFuncionario(@PathVariable("id") long id, Model model) {
+		Funcionario funcionario = FuncionarioRepository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("Identificador não é válido" + id));
+				FuncionarioRepository.delete(funcionario);
+		return "redirect:/Funcionario";
+	}
+		
 
 }
--- src/main/java/com/gerenciadordefuncionarios/model/Funcionario.java
+++ src/main/java/com/gerenciadordefuncionarios/model/Funcionario.java
@@ -1,21 +1,29 @@
 package com.gerenciadordefuncionarios.model;
 
 import javax.persistence.Entity;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+
 @Entity
+@Data
 public class Funcionario {
 			
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;	
 		
 
-	@NotBlank(message = "O nome do funcionario deve ser preenchido")// Avisa sobre o null
+	@NotBlank(message = "O nome do funcionario deve ser preenchido")
 	@Size (min= 3, max = 75)
 	private String nome;
 	
@@ -23,47 +31,5 @@ public class Funcionario {
 	private String cargo;
 	private String urlImagem;
 	
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome; 
-	
-	}
-		
-	public String getEmail() {
-		return email;
-	
-	}
-		
-	public void setEmail(String email) {
-		this.email = email;
-	}
-		
-		
-	public String getCargo() {
-		return cargo;
-	}
-	public void setCargo(String cargo) {
-		this.cargo = cargo;
-	}
-	
-	public String getUrlImagem() {
-		return urlImagem;
-	}
-	public void setUrlImagem(String urlImagem) {
-		this.urlImagem = urlImagem;
-	}
-	
-	
- 
-
 }
 	
